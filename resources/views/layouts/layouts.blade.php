14e518c0b9abd250c21f6ca6fd3253d8363a7be4
yosnak13 <yosnak20@gmail.com>
2020-11-16 08:54:32 +0900

    bootstrap4

---
+++ resources/views/layouts/layouts.blade.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>@yield('title')</title>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+
+         <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html> 
