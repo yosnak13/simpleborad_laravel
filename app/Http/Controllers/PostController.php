e6e21c07285e57acd178c157caa907fe845a60ad
yosnak13 <yosnak20@gmail.com>
2020-11-16 08:51:18 +0900

    destroyアクション、5章終了

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -90,6 +90,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return redirect()->route('posts.index');
     }
 }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,5 +2,12 @@
 @foreach($posts as $post)
   <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
   <a href="/posts/{{ $post->id }}/edit">Edit</a>
+
+  <form action="/posts/{{ $post->id }}" method="POST" onsubmit="if(confirm('Delete? Are you sure?')) { return true } else {return false };">
+      <input type="hidden" name="_method" value="DELETE">
+      <input type="hidden" name="_token" value="{{ csrf_token() }}">
+      <button type="submit">Delete</button>
+  </form>
 @endforeach
+
 <a href="/posts/create">New Post</a>
